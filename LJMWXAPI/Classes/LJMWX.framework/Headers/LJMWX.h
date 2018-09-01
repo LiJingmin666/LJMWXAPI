464d00652ee1cb94c122901e8cd6cd24aadf2331
LiJingmin <lijingmin@agapday.com>
2018-09-01 11:58:02 +0800

    add framework

---
+++ LJMWXAPI/Classes/LJMWX.framework/Headers/LJMWX.h
@@ -0,0 +1,19 @@
+//
+//  LJMWX.h
+//  LJMWX
+//
+//  Created by 李精敏 on 2018/9/1.
+//  Copyright © 2018年 李精敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LJMWX.
+FOUNDATION_EXPORT double LJMWXVersionNumber;
+
+//! Project version string for LJMWX.
+FOUNDATION_EXPORT const unsigned char LJMWXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LJMWX/PublicHeader.h>
+
+
